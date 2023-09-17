bf54ec842ded40d3509461a3f94f0759264ddcac
mkauley <user@arcanematrix.com>
2023-09-16 20:38:16 -0400

    Set up user logins

--- bit.py
+++ bit.py
@@ -1,12 +1,14 @@
 from os import path
 from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required
+from flask_login import logout_user, current_user, LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 # Global variable for database
 # BE SURE TO SWITCH THESE WHEN DOING LOCAL DEVELOPMENT VS. DEPLOYED VERSION
-#DB_NAME = "/home/ec2-user/CMSC-495-Project/cmsc495.db"
-DB_NAME = "cmsc495.db" #-- This is used when doing local testing.
+#DB_NAME = "/home/ec2-user/CMSC-495-Project/instance/cmsc495.db"
+DB_NAME = "instance/cmsc495.db" #-- This is used when doing local testing.
 
 bitwiz = Flask(__name__)
 bitwiz.config['SECRET_KEY'] = 'WeAreVeryMagical1357913'
@@ -25,8 +27,8 @@ def currentTime():
     dateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return dateTime
 
-class user(db.Model):
-    userId = db.Column(db.Integer, primary_key=True)
+class user(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     encryptedPassword = db.Column(db.String(200))
     salt = db.Column(db.String(50))
@@ -41,7 +43,7 @@ class user(db.Model):
         self.passwordRecoveryAnswer = passwordRecoveryAnswer
 
 class passwordEntry(db.Model):
-    entryId = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer)
     title = db.Column(db.String(100))
     encryptedPassword = db.Column(db.String(100))
@@ -51,7 +53,7 @@ class passwordEntry(db.Model):
     dateModified = db.Column(db.Time)
 
 class passwordGenerator(db.Model):
-    algorithimId = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     algorithim = db.Column(db.String(100))
     length = db.Column(db.Integer)
     useUppercase = db.Column(db.Boolean)
@@ -60,9 +62,17 @@ class passwordGenerator(db.Model):
     useSpeicalChars = db.Column(db.Boolean)
 
 class encryptiionHandler(db.Model):
-    algorithimTypeId = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     algorithmType = db.Column(db.String(100))
     encryptionKey = db.Column(db.String(100))
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+
+# User Loader for Login Manaager
+@login_manager.user_loader
+def load_user(id):
+    return user.query.get(id)
 
 @bitwiz.route('/register', methods=['POST', 'GET'])
 def indexPage():
@@ -73,16 +83,18 @@ def indexPage():
     answer = None
 
     if request.method == 'POST':
-        newUsername = request.form['username']
-        newPassword = request.form['password']
-        newSalt = request.form['salt']
-        newQuestion = request.form['question']
-        newAnswer = request.form['answer']
+        newUsername = request.form.get('username')
+        newPassword = request.form.get('password')
+        newSalt = request.form.get('salt')
+        newQuestion = request.form.get('question')
+        newAnswer = request.form.get('answer')
 
         newRec = user(newUsername, newPassword, newSalt, newQuestion, newAnswer)
         db.session.add(newRec)
         db.session.commit()
+        login_user(newRec, remember=True)
 
+        #TO DO -> FIGURE OUT WHAT PAGE SHOULD COME NEXT
         return redirect(url_for('nextPage'))
 
     return render_template('index.html', timestamp = currentTime(), title = 'CMST 495 - BitWizards')
@@ -106,6 +118,18 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
+        log_user = user.query.filter_by(username=username).first()
+
+        # Check for existing user before logging in
+        if log_user:
+            if log_user.encryptedPassword == password:
+                login_user(log_user, remember=True)
+                return redirect(url_for('nextPage'))
+            else:
+                flash('Incorrect Password')
+        else:
+            flash('User Not Found')
+
         # Add the logic for Login
 
     return render_template('login.html', timestamp = currentTime(), title = 'CMST 495 - BitWizards')
@@ -119,15 +143,18 @@ def masterReset():
     return render_template('ResetMasterPass.html')
 
 @bitwiz.route('/next', methods=['GET'])
+@login_required
 def nextPage():
 
     allRecords = user.query.all()
 
-    return render_template('next.html', records=allRecords, timestamp = currentTime(), title = 'Database Lookup')
+    flash('Hello There') #TESTLINE
 
+    return render_template('next.html', records=allRecords, timestamp = currentTime(), title = 'Database Lookup')
 
 if __name__ == '__main__':
     create_db(bitwiz)
+    login_manager.init_app(bitwiz)
 # BE SURE TO SWITCH THESE WHEN DOING LOCAL DEVELOPMENT VS. DEPLOYED VERSION
     #bitwiz.run()
-    bitwiz.run(debug=True)
+    bitwiz.run(debug=True)  #TESTLINE
