b37235f7cbcc738f27af2329f44af0d0491fbab2
mkauley <user@arcanematrix.com>
2023-09-25 20:38:36 -0400

    Added signed in user up top. Also removed the query page as the functionality was similar to next and query page was only ever a test place holder

--- bit.py
+++ bit.py
@@ -405,7 +405,8 @@ def pass_entry():
         db.session.add(new_pass)
         db.session.commit()
 
-        return redirect(url_for('query_page', user_val=curruser_id))
+#        return redirect(url_for('query_page', user_val=curruser_id))
+        return redirect(url_for('next_page'))
 
     return render_template('PasswordEntry.html', timestamp=current_time(),
                            title='CMST 495 - BitWizards - Create Password')
